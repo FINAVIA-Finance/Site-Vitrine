6be953d053259ffe367aa84df330afb99dc6b8bd
FINAVIA-Finance <contact@finavia.fr>
2026-05-14 19:30:27 +0200

    Align sitemap URL to non-www domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://www.finavia.fr";
+const siteUrl = "https://finavia.fr";
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const currentDate = new Date();
